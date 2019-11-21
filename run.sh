851830e870cca0f6d78bf2c94fbfc1b1b02cab3e
lrakai <lmrakai@gmail.com>
2019-11-21 00:16:29 -0700

    Add test Runner

---
+++ run.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+JUNIT_HOME=./lib/junit-4.10.jar
+CLASSES=./classes
+
+test=One
+javac -d $CLASSES src/*$test*.java
+javac -cp $JUNIT_HOME:$CLASSES -d $CLASSES test/*$test*.java
+
+java -cp $JUNIT_HOME:$CLASSES TestRunner$test
